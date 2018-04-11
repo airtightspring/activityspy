805aaa5cc9e188b8c7dc88f8c45b9e71afe54e2a
Kevin Jerauld <airtightspring3@gmail.com>
2018-04-11 14:04:13 -0700

    Initial commit

---
+++ app/src/main/java/activityspy/kjerauld/washington/edu/activityspy/MainActivity.kt
@@ -0,0 +1,29 @@
+package activityspy.kjerauld.washington.edu.activityspy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("InfoLog01", "onCreate event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("ErrorLog02", "We're going down, captain!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("InfoLog02", "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("InfoLog03", "onResume event fired")
+    }
+}
